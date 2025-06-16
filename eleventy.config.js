4df29b9c0b0559eaaf326e980903f3b989bb0e78
mxhert <mxhert@gmail.com>
2025-06-16 19:22:59 -0400

    new passthroug

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,11 +18,7 @@ export default async function(eleventyConfig) {
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
-	eleventyConfig
-		.addPassthroughCopy({
-			"./public/": "/"
-		})
-	//	.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+	eleventyConfig.addPassthroughCopy({ "public": "." });
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
